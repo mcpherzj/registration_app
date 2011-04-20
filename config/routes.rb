ca38ca87ee6ba7eb891a72ed5e345d3c3230a430
John McPherson <john@jrmcpherson.com>
2011-04-20 16:38:18 -0500

    Built out the initial app layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 RegistrationApp::Application.routes.draw do
-  get "pages/home"
+  #get "users/new"
 
-  get "pages/contact"
+  root :to => "pages#home"
 
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  #match '/help', :to => 'pages#help'
+  
+  #match '/signup', :to => 'users#new'
+  
+  #get "pages/home"
+
+  #get "pages/contact"
+
+  #get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
