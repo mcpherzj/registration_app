551538765e255c483517a8c6d77fcb49af211d3f
John McPherson <john@jrmcpherson.com>
2011-04-27 00:15:57 -0500

    Rack SSL Enforcer

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module RegistrationApp
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     
-    #config.force_ssl = true
+    # SSL enforcement
+    config.middleware.use Rack::SslEnforcer, :except => /^\/deploys/
   end
 end
