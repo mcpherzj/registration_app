050a5c6eab9a90b2655d0533fee5ebe33c37f84d
John McPherson <john@jrmcpherson.com>
2012-02-17 01:20:59 -0600

    fixed bug in season

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,7 +5,7 @@ class Season < ActiveRecord::Base
 	validates(:year, 	:presence => true,
 						:uniqueness => { :scope => :season_type_id })
 	validates(:season_type_id, 	:presence => true)
-	validates(:active, 	:presence => true)
+	validates(:active, :inclusion => { :in => [true, false] })
 	
 	def name
 		year.to_s + " " + season_type.name
